9936cb257ecb60cd76066318ddba5cb3cf0b9074
Marthilda <marthilda.vollmer@hotmail.de>
2024-11-04 11:44:44 +0100

    Change layout = fill to fill in Image component

--- components/PlantCard.js
+++ components/PlantCard.js
@@ -50,7 +50,7 @@ export default function PlantCard({ plant }) {
       <ImageWrapper>
         <Image
           alt={`Image of ${plant.name}`}
-          layout="fill"
+          fill
           src={
             plant.imageUrl ||
             "https://images.unsplash.com/photo-1564502983799-becfbf817b4f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
