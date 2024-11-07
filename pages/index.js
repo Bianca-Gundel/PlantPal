0d3bdaab434c89c077ae36e20f8ebf352ab54ce9
Christina Bürger <christina.buerger@outlook.de>
2024-11-07 15:23:32 +0100

    add styling for error message

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import PlantCard from "@/components/PlantCard";
+import Image from "next/image";
 import styled from "styled-components";
 
 const StyledPlantList = styled.ul`
   list-style: none;
   padding: 0;
+`;
+
+const StyledErrorMessageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
 `;
 
 export default function HomePage({ plants }) {
@@ -20,7 +29,16 @@ export default function HomePage({ plants }) {
           ))}
         </StyledPlantList>
       ) : (
-        <p>Unfortunately, you have not yet added any plants.</p>
+        <StyledErrorMessageWrapper>
+          <Image
+            src={"/icons/error-plant.svg"}
+            width={50}
+            height={50}
+            alt="Icon of a dead plant"
+            unoptimized
+          />
+          <p>Unfortunately, you have not yet added any plants.</p>
+        </StyledErrorMessageWrapper>
       )}
     </>
   );
