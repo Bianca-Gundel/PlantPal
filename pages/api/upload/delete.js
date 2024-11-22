7ab65c9ea5222a27a2373333db5355d34674a0ae
Marthilda <marthilda.vollmer@hotmail.de>
2024-11-22 16:57:53 +0100

    Minor changes

--- pages/api/upload/delete.js
+++ pages/api/upload/delete.js
@@ -6,9 +6,9 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET,
 });
 
-export default async function handler(req, res) {
-  if (req.method === "DELETE") {
-    const { imageUrl } = req.body;
+export default async function handler(request, response) {
+  if (request.method === "DELETE") {
+    const { imageUrl } = request.body;
     const publicId = imageUrl
       .replace("https://res.cloudinary.com/", "")
       .replace(".jpg", "")
@@ -16,9 +16,9 @@ export default async function handler(req, res) {
 
     cloudinary.v2.uploader.destroy(publicId, (error, result) => {
       if (error) {
-        res.status(500).json({ message: "Error deleting image" });
+        response.status(500).json({ message: "Error deleting image" });
       } else {
-        res.status(200).json({ message: "Image deleted successfully" });
+        response.status(200).json({ message: "Image deleted successfully" });
       }
     });
   } else {
